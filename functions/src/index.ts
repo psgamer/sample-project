75ab28fb403bb154af9ce4e12b1446e51107d473
andrey.tkachov <andrey.tkachov@genixsolutions.biz>
2023-09-14 18:16:14 +0300

    upgrade to angular 16, eslint added with default configs (remake later)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,34 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+// import {logger} from "firebase-functions";
+// import {user, UserRecord} from 'firebase-functions/lib/providers/auth';
+// // import {AuthUserRecord} from "firebase-functions/lib/common/providers/identity";
+// // import {user, UserRecord} from "firebase-functions/lib/v1/providers/auth";
+// // import {onRequest} from "firebase-functions/lib/v2/providers/https";
+// // import {UserRecord} from "firebase-functions/lib/common/providers/identity";
+// // import * as logger from "firebase-functions/logger";
+// // import {user, UserRecord } from "firebase-functions/v1/auth";
+// import {onRequest} from "firebase-functions/v2/https";
+//
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+//
+// export const onCreateUser = user().onCreate((user: UserRecord) => {
+//   logger.info("User signed in!", {structuredData: true});
+//   return user;
+// });
+//
+// // export const onAuthUser = user().beforeSignIn((user: AuthUserRecord) => {
+// //   logger.info("new sign in before");
+// //   return user;
+// // });
+
 import { UserRecord } from "firebase-functions/lib/common/providers/identity";
 import * as logger from "firebase-functions/logger";
 import { user } from "firebase-functions/v1/auth";
@@ -16,11 +44,20 @@ import { onRequest } from "firebase-functions/v2/https";
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
 });
 
 export const onAuth = user().onCreate((user: UserRecord) => {
-  logger.info("User signed in!", {structuredData: true});
-  return user;
+    logger.info("User signed in!", {structuredData: true});
+    return user;
+});
+
+export const onAuthUser = user().beforeSignIn(() => {
+  logger.info("new sign in before");
+  return {
+      customClaims: {
+          customData: 'somestr',
+      }
+  };
 });
