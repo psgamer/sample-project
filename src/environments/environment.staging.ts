12f54ec7f7b1925087e137dce01a788f3161564a
andrey.tkachov <andrey.tkachov@genixsolutions.biz>
2023-09-15 16:08:12 +0300

    added separate staging config

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,16 @@
+import { EnvironmentConfig } from './environment.types';
+
+export const environment: EnvironmentConfig = {
+    firebase: {
+        projectId: 'project-sample-8e517',
+        appId: '1:1043809573694:web:e7dd1f2c5113397675aa6f',
+        storageBucket: 'project-sample-8e517.appspot.com',
+        apiKey: 'AIzaSyAlkUIYvxm5RUSe-PlWGIvX2yEO6Rwpdv0',
+        authDomain: 'project-sample-8e517.firebaseapp.com',
+        messagingSenderId: '1043809573694',
+        measurementId: 'G-LJYH7NV4ZL',
+        databaseURL: 'https://project-sample-8e517-default-rtdb.europe-west1.firebasedatabase.app',
+    },
+    production: true,
+    useEmulators: false,
+};
